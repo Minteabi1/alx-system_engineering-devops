e16bc7faac52b05fae07166988052173ebd5edea
Minteabi1 <mintesnottsegaye40@gmail.com>
2023-01-29 11:30:21 +0300

    update file permission

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,43 +1,34 @@
 #!/usr/bin/python3
-"""Given an Employee ID, returns information
-about his/her TODO list progress.
-"""
+""" Gather Data From Api """
+
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    try:
-        emp_id = int(argv[1])
-    except ValueError:
-        exit()
 
-    api_url = 'https://jsonplaceholder.typicode.com'
-    user_uri = '{api}/users/{id}'.format(api=api_url, id=emp_id)
-    todo_uri = '{user_uri}/todos'.format(user_uri=user_uri)
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print("Usage: ./0-gather_data_from_an_API.py <employee ID>")
+        exit(1)
+    id = sys.argv[1]
 
-    # User Response
-    res = requests.get(user_uri).json()
+    def get_user_info(id):
+        """ Get User Info """
+        api_url = 'https://jsonplaceholder.typicode.com'
+        user_uri = '{api}/users/{id}'.format(api=api_url, id=id)
+        ruser_uri = requests.get(user_uri)
+        return ruser_uri.json()
 
-    # Name of the employee
-    name = res.get('name')
-
-    # User TODO Response
-    res = requests.get(todo_uri).json()
-
-    # Total number of tasks, the sum of completed and non-completed tasks
-    total = len(res)
-
-    # Number of non-completed tasks
-    non_completed = sum([elem['completed'] is False for elem in res])
-
-    # Number of completed tasks
-    completed = total - non_completed
-
-    # Formatting the expected output
-    str = "Employee {emp_name} is done with tasks({completed}/{total}):"
-    print(str.format(emp_name=name, completed=completed, total=total))
-
-    # Printing completed tasks
-    for elem in res:
-        if elem.get('completed') is True:
-            print('\t', elem.get('title'))
+    def get_user_tasks(id):
+        """ Get User Tasks """
+        api_url = 'https://jsonplaceholder.typicode.com'
+        user_uri = '{api}/users/{id}'.format(api=api_url, id=id)
+        todo_uri = '{user_uri}/todos'.format(user_uri=user_uri)
+        todo_url = requests.get(todo_uri)
+        return todo_url.json()
+    user = get_user_info(id)
+    tasks = get_user_tasks(id)
+    completed_tasks = []
+    [completed_tasks.append(task) for task in tasks if task.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed_tasks), len(tasks)))
+    [print("\t {}".format(task.get("title"))) for task in completed_tasks]
